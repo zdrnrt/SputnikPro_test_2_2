cbace62e8a295402a81bc8fb474058979aeb5d70
Задорин Артем <artfan95@gmail.com>
2025-05-06 23:59:13 +0700

    Добавлен график на вкладку Промо коэффициенты

--- src/js/modules/promo_ratio.js
+++ src/js/modules/promo_ratio.js
@@ -1,3 +1,6 @@
+import * as XLSX from 'xlsx';  // ES6-импорт
+import Plotly from 'plotly.js-dist-min';
+
 export default window.showContent_promoRatio = function() {
     fetch('./src/html/promo_ratio.html')
         .then(response => {
@@ -8,11 +11,93 @@ export default window.showContent_promoRatio = function() {
         })
         .then(html => {
             document.getElementById('mainContent').innerHTML = html;
+            promoRatioInit();
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
         });
+}
+
+function promoRatioInit(){
+    document.getElementById('promoRatio_calculation').addEventListener('click', promoRatioChart)
+}
+
+
+function promoRatioChart(){
+    const dataPath = {
+        average: './data/promoRatio/promoCoefficientsAvg.xlsx',
+        mediana: './data/promoRatio/promoCoefficientsMedian.xlsx'
     }
+    const value = document.getElementById('promoRatio_method').value;
+    const path = dataPath[value]
+
+    document.getElementById('download-container').classList.remove('d-none');
+    document.getElementById('download').href = path;
+    
+    console.log('promoRatioChart')
+
+    fetch(path)
+        .then(response => response.arrayBuffer())
+        .then(data => {
+            const workbook = XLSX.read(data, { type: 'array' });
+            const firstSheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[firstSheetName];
+            const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+            const labels = [];
+            const datasets = {};
+            console.log('json', jsonData)
+            // Пропускаем заголовок и обрабатываем данные
+            for (let i = 1; i < jsonData.length; i++) {
+                const row = jsonData[i];
+                if (row.length === 3) { // Убедитесь, что в строке три колонки
+                    const productType = row[0]; // Наименование
+                    const segment = row[1]; // Ценовой сегмент
+                    const value = parseFloat(row[2]); // Коэффициенты
+                    // Добавляем сегмент в labels, если их еще нет и если неделя не -1
+                    if (!labels.includes(segment) && segment !== '-1') {
+                        labels.push(segment);
+                    }
+                    // Создаем массив для каждого типа продукции, если его еще нет
+                    if (!datasets[productType]) {
+                        datasets[productType] = { 
+                            x: [], 
+                            y: [], 
+                            type: 'scatter', // Тип графика
+                            mode: 'lines+markers', // Режим отображения
+                            name: productType // Имя для легенды
+                        };
+                    }
+                    const segmentIndex = labels.indexOf(segment); // Заполняем массив значений для соответствующей недели
+                    if (segmentIndex !== -1) {
+                        datasets[productType].x.push(segment);
+                        datasets[productType].y.push(value);
+                    }
+                }
+            }
+            // Преобразуем объект datasets в массив для Plotly
+            const plotData = Object.values(datasets);
+            // Создание нового графика
+            Plotly.newPlot('chart', plotData, {
+                title: 'Промо коэффициенты',
+                xaxis: {
+                    title: 'Ценовой сегмент',
+                    tickmode: 'linear', // Используем линейный режим для оси X
+                    tick0: 0, // Начальное значение оси X (Ценовой сегмент)
+                    dtick: 1, // Шаг по оси X
+                    range: [-0.5, 4.5]
+                },
+                yaxis: {
+                    title: 'Значение',
+                    // rangemode: 'tozero' // Начинаем ось Y с нуля
+                    range: [0.5, 2.5]
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при загрузке файла:', error);
+        });
+
+}
 
 //   // Находим главный чекбокс и все подчиненные чекбоксы по data-атрибуту
 //   const mainCheckbox = document.querySelector('.main-checkbox[data-group="metrics"]');
